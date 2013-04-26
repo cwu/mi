f97829cd4ff26aa661e36a9fdff223e0a8a5ba02
Wien Leung <->
2013-04-25 20:23:06 -0400

    can coerce input to one reference

--- filter.py
+++ filter.py
@@ -11,43 +11,53 @@ if len(sys.argv) != 3:
 inp = json.loads(open(sys.argv[1]).read())
 des = json.loads(open(sys.argv[2]).read())
 
-# Create train samples
-#i1 = np.sin(np.arange(0, 20))
-#i2 = np.sin(np.arange(0, 20)) * 2
-#
-#t1 = np.ones([1, 20])
-#t2 = np.ones([1, 20]) * 2
-#
-#input = np.array([i1, i2, i1, i2]).reshape(20 * 4, 1)
-#target = np.array([t1, t2, t1, t2]).reshape(20 * 4, 1)
-
+# Make lists of deltas
 input = inp['path']
-print input
 input = [[x[0] - input[i - 1][0], x[1] - input[i - 1][1]] for i, x in enumerate(input)][1:]
-print input
-target = input
+target = des['path']
+target = [[x[0] - target[i - 1][0], x[1] - target[i - 1][1]] for i, x in enumerate(target)][1:]
 
 # Create network with 2 layers
-net = nl.net.newelm([[-8, 8], [-8, 8]], [10, 2], [nl.trans.TanSig(), nl.trans.PureLin()])
+net = nl.net.newelm([[-8, 8], [-8, 8]], [20, 2], [nl.trans.TanSig(), nl.trans.PureLin()])
 # Set initialized functions and init
 net.layers[0].initf = nl.init.InitRand([-0.1, 0.1], 'wb')
 net.layers[1].initf = nl.init.InitRand([-0.1, 0.1], 'wb')
 net.init()
 # Train network
-error = net.train(input, target, epochs=5000, show=100, goal=0.0001)
+error = net.train(target, target, epochs=1000, show=100, goal=0.0001)
 # Simulate network
 output = net.sim(input)
 
 # Plot result
 import pylab as pl
-pl.subplot(211)
+pl.subplot(311)
 pl.plot(error)
 pl.xlabel('Epoch number')
 pl.ylabel('Train error (default MSE)')
 
-pl.subplot(212)
-pl.plot(target)
-pl.plot(output)
-pl.legend(['train target', 'net output'])
+range = 200
+pl.subplot(312)
+pl.plot([x[0] for x in target][:range])
+pl.plot([x[0] for x in input][:range])
+pl.plot([x[0] for x in output][:range])
+pl.legend(['train x', 'input x', 'output x'])
+
+pl.subplot(313)
+pl.plot([x[1] for x in target][:range])
+pl.plot([x[1] for x in input][:range])
+pl.plot([x[1] for x in output][:range])
+pl.legend(['train y', 'input x', 'output y'])
+
 pl.show()
 pl.savefig('test.png')
+
+# Dump out output JSON
+data = [inp['path'][0]]
+for x in output:
+  last = data[-1]
+  data.append([last[0] + x[0], last[1] + x[1]])
+
+outjson = inp
+outjson['path'] = data
+with open('out.json', 'w') as outfile:
+  json.dump(outjson, outfile)
